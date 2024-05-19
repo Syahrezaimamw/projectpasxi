490eb31f70cd2a82995c9d104a0493ffb35266cb
Syahrezaimamw <syahrezaimamwahyudi976@gmail.com>
2024-05-19 20:23:05 +0700

    da

--- src/components/Laodlogin.jsx
+++ src/components/Laodlogin.jsx
@@ -4,7 +4,7 @@ const Laodlogin = () => {
   return (
     <div className='flex items-end justify-center '>
       Loading 
-      <span className="loading loading-dots loading-xs ms-[1px]"></span>
+      <span className="loading loading-dots loading-xs ms-[1px]">...</span>
     </div>
   )
 }
